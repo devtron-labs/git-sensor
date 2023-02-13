373d02a146b853e22908842edeced8de4890633d
ayushmaheshwari <ayush@devtron.ai>
2023-02-13 20:15:05 +0530

    migration script correction

--- scripts/sql/7_alter_webhook_in_ci.up.sql
+++ scripts/sql/7_alter_webhook_in_ci.up.sql
@@ -1,11 +1,5 @@
 INSERT INTO git_host_webhook_event_selectors (event_id, name, selector, to_show, to_show_in_ci_filter, is_active, possible_values, created_on)
-VALUES (1, 'repository ssh url', 'repository.ssh_url', 'f', 'f', 't', NULL, NOW());
-
-INSERT INTO git_host_webhook_event_selectors (event_id, name, selector, to_show, to_show_in_ci_filter, is_active, possible_values, created_on)
-VALUES (2, 'repository ssh url', 'repository.ssh_url', 'f', 'f', 't', NULL, NOW());
-
-INSERT INTO git_host_webhook_event_selectors (event_id, name, selector, to_show, to_show_in_ci_filter, is_active, possible_values, created_on)
-VALUES (3, 'repository ssh url', 'repository.ssh_url', 'f', 'f', 't', NULL, NOW());
-
-INSERT INTO git_host_webhook_event_selectors (event_id, name, selector, to_show, to_show_in_ci_filter, is_active, possible_values, created_on)
-VALUES (4, 'repository ssh url', 'repository.ssh_url', 'f', 'f', 't', NULL, NOW());
+VALUES (1, 'repository ssh url', 'repository.ssh_url', 'f', 'f', 't', NULL, NOW()),
+       (2, 'repository ssh url', 'repository.ssh_url', 'f', 'f', 't', NULL, NOW()),
+       (3, 'repository ssh url', 'repository.ssh_url', 'f', 'f', 't', NULL, NOW()),
+       (4, 'repository ssh url', 'repository.ssh_url', 'f', 'f', 't', NULL, NOW());
