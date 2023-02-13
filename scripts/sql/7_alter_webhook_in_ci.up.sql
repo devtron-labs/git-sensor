f83ba09a550f7ea7bcb2e945953322215044f4dc
ayushmaheshwari <ayush@devtron.ai>
2023-02-13 18:19:23 +0530

    migration scripts

--- scripts/sql/7_alter_webhook_in_ci.up.sql
+++ scripts/sql/7_alter_webhook_in_ci.up.sql
@@ -1,2 +1,5 @@
 INSERT INTO git_host_webhook_event_selectors (event_id, name, selector, to_show, to_show_in_ci_filter, is_active, possible_values, created_on)
 VALUES (1, 'repository ssh url', 'repository.ssh_url', 'f', 'f', 't', NULL, NOW());
+
+INSERT INTO git_host_webhook_event_selectors (event_id, name, selector, to_show, to_show_in_ci_filter, is_active, possible_values, created_on)
+VALUES (2, 'repository ssh url', 'repository.ssh_url', 'f', 'f', 't', NULL, NOW());
