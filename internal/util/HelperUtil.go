3df190b7a00e7176c428f3a64fa039b30e50c358
Manish Agrawal <manish@devtron.ai>
2023-02-16 12:25:13 +0530

    extra env variable key logic change

--- internal/util/HelperUtil.go
+++ internal/util/HelperUtil.go
@@ -1,15 +1,19 @@
 package util
 
 import (
-	"fmt"
 	"github.com/devtron-labs/git-sensor/internal/sql"
+	"strings"
 )
 
 func BuildExtraEnvironmentVariablesForCi(filterResults []*sql.CiPipelineMaterialWebhookDataMappingFilterResult) map[string]string {
 	extraEnvironmentVariables := make(map[string]string)
 	for _, filterResult := range filterResults {
 		for k, v := range filterResult.MatchedGroups {
-			extraEnvironmentVariables[fmt.Sprintf("%s_%s", filterResult.SelectorName, k)] = v
+			// upper case
+			key := strings.ToUpper(k)
+			// replace space with underscore
+			key = strings.ReplaceAll(key, " ", "_")
+			extraEnvironmentVariables[key] = v
 		}
 	}
 	return extraEnvironmentVariables
