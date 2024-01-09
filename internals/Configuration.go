11a519b21c9d097801fcff04220c188620c67325
ShashwatDadhich <dadhichshashwat1808@gmail.com>
2024-01-09 12:43:10 +0530

    internal name changes to internals

--- internals/Configuration.go
+++ internals/Configuration.go
@@ -5,11 +5,12 @@ import (
 )
 
 type Configuration struct {
-	CommitStatsTimeoutInSec int  `env:"COMMIT_STATS_TIMEOUT_IN_SEC" envDefault:"2"`
-	EnableFileStats         bool `env:"ENABLE_FILE_STATS" envDefault:"false"`
-	GitHistoryCount         int  `env:"GIT_HISTORY_COUNT" envDefault:"15"`
-	MinLimit                int  `env:"MIN_LIMIT_FOR_PVC" envDefault:"1"` // in MB
-	UseGitCli               bool `env:"USE_GIT_CLI" envDefault:"false"`
+	CommitStatsTimeoutInSec int    `env:"COMMIT_STATS_TIMEOUT_IN_SEC" envDefault:"2"`
+	EnableFileStats         bool   `env:"ENABLE_FILE_STATS" envDefault:"false"`
+	GitHistoryCount         int    `env:"GIT_HISTORY_COUNT" envDefault:"15"`
+	CloningMode             string `env:"CLONING_MODE" envDefault:"FULL"`
+	MinLimit                int    `env:"MIN_LIMIT_FOR_PVC" envDefault:"1"` // in MB
+	UseGitCli               bool   `env:"USE_GIT_CLI" envDefault:"false"`
 }
 
 func ParseConfiguration() (*Configuration, error) {
