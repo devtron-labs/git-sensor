25c3eb64bb62b0208662badf77f1f2d510659732
subhashish-devtron <129164905+subhashish-devtron@users.noreply.github.com>
2024-01-24 11:55:55 +0530

    fixes-cli (#82)

--- pkg/git/GitFormatter.go
+++ pkg/git/GitFormatter.go
@@ -1,16 +1,36 @@
 package git
 
-import "time"
+import (
+	"encoding/json"
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+)
 
-var GITFORMAT = `--pretty=format:{
-  "commit": "%H",
-  "parent": "%P",
-  "refs": "%D",
-  "subject": "%s",
-  "body": "%b",
-  "author": { "name": "%aN", "email": "%aE", "date": "%ad" },
-  "commiter": { "name": "%cN", "email": "%cE", "date": "%cd" }
- },`
+const _dl_ = "devtron_delimiter"
+
+// GITFORMAT Refer git official doc for supported placeholders to add new fields
+// Need to make sure the output does not break the json structure which is ensured
+// by having the _dl_ delimiter which is later replaced by quotes
+var GITFORMAT = "--pretty=format:{" +
+	_dl_ + "commit" + _dl_ + ":" + _dl_ + "%H" + _dl_ + "," +
+	_dl_ + "parent" + _dl_ + ":" + _dl_ + "%P" + _dl_ + "," +
+	_dl_ + "refs" + _dl_ + ":" + _dl_ + "%D" + _dl_ + "," +
+	_dl_ + "subject" + _dl_ + ":" + _dl_ + "%s" + _dl_ + "," +
+	_dl_ + "body" + _dl_ + ":" + _dl_ + "%b" + _dl_ + "," +
+	_dl_ + "author" + _dl_ +
+	":{" +
+	_dl_ + "name" + _dl_ + ":" + _dl_ + "%aN" + _dl_ + "," +
+	_dl_ + "email" + _dl_ + ":" + _dl_ + "%aE" + _dl_ + "," +
+	_dl_ + "date" + _dl_ + ":" + _dl_ + "%ad" + _dl_ +
+	"}," +
+	_dl_ + "commiter" + _dl_ +
+	":{" +
+	_dl_ + "name" + _dl_ + ":" + _dl_ + "%cN" + _dl_ + "," +
+	_dl_ + "email" + _dl_ + ":" + _dl_ + "%cE" + _dl_ + "," +
+	_dl_ + "date" + _dl_ + ":" + _dl_ + "%cd" + _dl_ +
+	"}},"
 
 type GitPerson struct {
 	Name  string    `json:"name"`
@@ -23,5 +43,49 @@ type GitCommitFormat struct {
 	Refs     string    `json:"refs"`
 	Subject  string    `json:"subject"`
 	Commiter GitPerson `json:"commiter"`
+	Author   GitPerson `json:"author"`
 	Body     string    `json:"body"`
+}
+
+func parseFormattedLogOutput(out string) ([]GitCommitFormat, error) {
+	//remove the new line character which is after each terminal comma
+	out = strings.ReplaceAll(out, "},\n", "},")
+
+	// to escape the special characters like quotes and newline characters in the commit data
+	logOut := strconv.Quote(out)
+
+	//replace the delimiter with quotes to make it parsable json
+	logOut = strings.ReplaceAll(logOut, _dl_, `"`)
+
+	logOut = logOut[1 : len(logOut)-2]   // trim surround characters (surrounding quotes and trailing comma)
+	logOut = fmt.Sprintf("[%s]", logOut) // Add []
+
+	var gitCommitFormattedList []GitCommitFormat
+	err := json.Unmarshal([]byte(logOut), &gitCommitFormattedList)
+	if err != nil {
+		return nil, err
+	}
+	return gitCommitFormattedList, nil
+}
+
+func (formattedCommit GitCommitFormat) transformToCommit() *Commit {
+	return &Commit{
+		Hash: &Hash{
+			Long: formattedCommit.Commit,
+		},
+		Author: &Author{
+			Name:  formattedCommit.Author.Name,
+			Email: formattedCommit.Author.Email,
+			Date:  formattedCommit.Author.Date,
+		},
+		Committer: &Committer{
+			Name:  formattedCommit.Commiter.Name,
+			Email: formattedCommit.Commiter.Email,
+			Date:  formattedCommit.Commiter.Date,
+		},
+		Tag:     &Tag{},
+		Tree:    &Tree{},
+		Subject: formattedCommit.Subject,
+		Body:    formattedCommit.Body,
+	}
 }
