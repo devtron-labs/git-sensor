7d23e23c2a60c7b57bc7cc5939eefb7ff8ab450b
ayu-devtron <ayush.singh@devtron.ai>
2024-08-02 17:28:03 +0530

    update flag name and config.md

--- internals/Configuration.go
+++ internals/Configuration.go
@@ -19,16 +19,16 @@ package internals
 import "github.com/caarlos0/env"
 
 type Configuration struct {
-	CommitStatsTimeoutInSec      int    `env:"COMMIT_STATS_TIMEOUT_IN_SEC" envDefault:"2"`
-	EnableFileStats              bool   `env:"ENABLE_FILE_STATS" envDefault:"false"`
-	GitHistoryCount              int    `env:"GIT_HISTORY_COUNT" envDefault:"15"`
-	MinLimit                     int    `env:"MIN_LIMIT_FOR_PVC" envDefault:"1"` // in MB
-	UseGitCli                    bool   `env:"USE_GIT_CLI" envDefault:"false"`
-	UseAnalyticsCommitDiffGitCli bool   `env:"USE_ANALYTICS_COMMIT_DIFF_GIT_CLI" envDefault:"false"`
-	AnalyticsDebug               bool   `env:"ANALYTICS_DEBUG" envDefault:"false"`
-	CliCmdTimeoutGlobal          int    `env:"CLI_CMD_TIMEOUT_GLOBAL_SECONDS" envDefault:"0"`
-	CliCmdTimeoutJson            string `env:"CLI_CMD_TIMEOUT_JSON" envDefault:""`
-	GoGitTimeout                 int    `env:"GOGIT_TIMEOUT_SECONDS" envDefault:"10"`
+	CommitStatsTimeoutInSec int    `env:"COMMIT_STATS_TIMEOUT_IN_SEC" envDefault:"2"`
+	EnableFileStats         bool   `env:"ENABLE_FILE_STATS" envDefault:"false"`
+	GitHistoryCount         int    `env:"GIT_HISTORY_COUNT" envDefault:"15"`
+	MinLimit                int    `env:"MIN_LIMIT_FOR_PVC" envDefault:"1"` // in MB
+	UseGitCli               bool   `env:"USE_GIT_CLI" envDefault:"false"`
+	UseGitCliAnalytics      bool   `env:"USE_GIT_CLI_ANALYTICS" envDefault:"false"` // This flag is used to compute commitDiff using git-cli only for analytics
+	AnalyticsDebug          bool   `env:"ANALYTICS_DEBUG" envDefault:"false"`
+	CliCmdTimeoutGlobal     int    `env:"CLI_CMD_TIMEOUT_GLOBAL_SECONDS" envDefault:"0"`
+	CliCmdTimeoutJson       string `env:"CLI_CMD_TIMEOUT_JSON" envDefault:""`
+	GoGitTimeout            int    `env:"GOGIT_TIMEOUT_SECONDS" envDefault:"10" `
 }
 
 func ParseConfiguration() (*Configuration, error) {
